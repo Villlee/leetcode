3a2612181951b1c95bffba8bfad503808c6d0035
Villlee <15388605742@163.com>
2021-01-28 20:35:29 +0800

    2

--- 26.删除排序数组中的重复项.js
+++ 26.删除排序数组中的重复项.js
@@ -10,7 +10,15 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-
+    let j = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] != nums[j]){
+            j++
+        }
+        nums[j] = nums[i]
+    }
+    return j+1;
 };
+removeDuplicates([1,1,2])
 // @lc code=end
 
