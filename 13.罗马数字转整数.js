21996bb4825345fcc077178525a996ab2170603c
Villlee <15388605742@163.com>
2023-11-18 22:03:19 +0800

    feat: 第13题

--- 13.罗马数字转整数.js
+++ 13.罗马数字转整数.js
@@ -1,4 +1,14 @@
 /*
+ * @Author: Villlee 15388605742@163.com
+ * @Date: 2023-11-18 22:01:45
+ * @LastEditors: Villlee 15388605742@163.com
+ * @LastEditTime: 2023-11-18 22:02:49
+ * @FilePath: /leetcode/13.罗马数字转整数.js
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+/*
  * @lc app=leetcode.cn id=13 lang=javascript
  *
  * [13] 罗马数字转整数
@@ -9,7 +19,25 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-
+var romanToInt = function (s) {
+  const symbolValues = new Map();
+  symbolValues.set("I", 1);
+  symbolValues.set("V", 5);
+  symbolValues.set("X", 10);
+  symbolValues.set("L", 50);
+  symbolValues.set("C", 100);
+  symbolValues.set("D", 500);
+  symbolValues.set("M", 1000);
+  let ans = 0;
+  const n = s.length;
+  for (let i = 0; i < n; ++i) {
+    const value = symbolValues.get(s[i]);
+    if (i < n - 1 && value < symbolValues.get(s[i + 1])) {
+      ans -= value;
+    } else {
+      ans += value;
+    }
+  }
+  return ans;
 };
 // @lc code=en
